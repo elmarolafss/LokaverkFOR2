219e786ce03bdd8d5b9ac0406ec84c6df9ac35e9
elmarolafss <elmarol250@nemi.tskoli.is>
2017-04-25 15:10:27 +0000

    Prenta hrútana í python

--- Lokaverkefni.py
+++ Lokaverkefni.py
@@ -1,2 +1,8 @@
+spil = []
 with open("hrutar.txt", "r") as hrutarnir:
-    print(hrutarnir.read())
+    for line in hrutarnir:
+        spil.append(line)
+print("Hrútar:\n")
+for line in spil:
+    print("Nafn, Þyngd, Mjólkurlagnir, Ull, Afkvæmi, Einkunn Læris, Frjósemi, Gerð/þykkt bakvöðva, Einkun fyrir malir ")
+    print(line)
