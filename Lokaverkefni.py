888c07c3078c5e56d250e75650e672008977c5c0
elmarolafss <elmarol250@nemi.tskoli.is>
2017-04-27 18:08:53 +0000

    Lagaði list of strings vandann

--- Lokaverkefni.py
+++ Lokaverkefni.py
@@ -1,14 +1,12 @@
 import random
-import csv
-from copy import *
-listoflists = []
+from ast import literal_eval
+
 spil = []
-with open("hrutar.txt", "r") as hrutarnir:
-    for line in csv.reader(hrutarnir):
-        spil.append(line)
-        listoflists.append((spil[:], spil[0]))
-print(spil)
-print(listoflists)
+with open("hrutar.txt") as hrutarnir:
+    for line in hrutarnir:
+        line = line.strip('\r\n')
+        spil.append(literal_eval(line))
+
 print("Hrútar:\n")
 for line in spil:
     print("Nafn, Þyngd, Mjólkurlagnir, Ull, Afkvæmi, Einkunn Læris, Frjósemi, Gerð/þykkt bakvöðva, Einkun fyrir malir ")
@@ -31,20 +29,33 @@ for item in spil:
     counter += 1
     if counter == 26:
         break
-
 print("\nSpiunum hafa verið skipt í tvennt")
 
 byrjun = random.randint(1,2)
 
+
 if byrjun == 1:
     print("\nNotandi Byrjar")
+    print("Flokkar:"
+          "1. Þyngd"
+          "2. Mjolkurlagning"
+          "3. Ull"
+          "4. Afkvæmi"
+          "5. Einkun Læris"
+          "6. Frjósemi"
+          "7. Gerð/Þykkt Bakvöðva"
+          "8. Einkun fyrir malir")
     val = int(input("Veldu flokk(1-8) til að keppst með: "))
 
     while val:
-        print("Notandinn: %s" % notandi[0][val])
+        print("Notandinn: %s" % notandi[0])
         print("Tölva: %s" % tolva[0])
         if notandi[0][val] > tolva[0][val]:
             print("Spil notandanns vann")
+            notandi.append(tolva[0])
+            tolva.remove(tolva[0])
         else:
             print("Spil Tölvunar vann")
+            tolva.append(notandi[0])
+            notandi.remove(notandi[0])
         break
